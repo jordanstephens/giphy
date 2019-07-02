1daf3be056213ccf56e111022e4ad35556166a74
Jordan Stephens <jordan@stephens.io>
2019-07-01 21:15:39 -0500

    setup configuration

---
+++ src/configuration.js
@@ -0,0 +1,22 @@
+/*global process*/
+
+const configuration = {
+  api_key: {
+    development: process.env.REACT_APP_API_KEY,
+    test: 'test-api-key',
+    production: process.env.REACT_APP_API_KEY
+  }
+};
+
+const environments = ['development', 'test', 'production'];
+
+function resolve(definition) {
+  return Object.keys(definition).reduce((acc, k) => {
+    const short = !environments.some((env) => definition[k].hasOwnProperty(env));
+    const value = short ? definition[k] : definition[k][process.env.NODE_ENV];
+    acc[k] = value;
+    return acc;
+  }, {});
+}
+
+export default resolve(configuration);
