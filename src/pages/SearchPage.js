21a6ab86b289f2512c359df46d997c20aafee535
Jordan Stephens <jordan@stephens.io>
2019-07-01 23:12:13 -0500

    create SearchPage

---
+++ src/pages/SearchPage.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import QueryString from 'query-string';
+import { connect } from 'react-redux';
+import { actions as searchActions } from '../features/search';
+import { bindActionCreators } from 'redux';
+import ContentContainer from '../containers/ContentContainer';
+import GifList from '../components/GifList';
+
+class SearchPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.loadMore = this.loadMore.bind(this);
+  }
+
+  componentDidMount() {
+    const { actions, location } = this.props;
+    const { q } = QueryString.parse(location.search.slice(1));
+    actions.reset();
+    actions.loadNextPage({ q });
+  }
+
+  loadMore() {
+    const { actions, location } = this.props;
+    const { q } = QueryString.parse(location.search.slice(1));
+    actions.loadNextPage({ q });
+  }
+
+  render() {
+    const { gifs, loading, error } = this.props;
+    return (
+      <ContentContainer loading={loading} error={error}>
+        <GifList
+          gifs={gifs}
+          onScrollBottom={this.loadMore}
+        />
+      </ContentContainer>
+    );
+  }
+}
+
+SearchPage.propTypes = {
+  actions: PropTypes.shape({
+    loadNextPage: PropTypes.func.isRequired,
+    reset: PropTypes.func.isRequired,
+  }).isRequired,
+  location: PropTypes.shape({
+    search: PropTypes.string.isRequired
+  }).isRequired,
+  gifs: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.shape({})
+};
+
+const mapStateToProps = ({ search }) => ({
+  loading: search.loading,
+  error: search.error,
+  gifs: search.gifs
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(searchActions, dispatch),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SearchPage);
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import MissingPage from './pages/MissingPage';
 import TrendingPage from './pages/TrendingPage';
+import SearchPage from './pages/SearchPage';
 
 const routes = (
   <Switch>
     <Route exact path='/' component={TrendingPage} />
+    <Route exact path='/search' component={SearchPage} />
     <Route component={MissingPage} />
   </Switch>
 );
